3f2218762a6bf1548539be00862b026d49bcf7de
Nick Plante <nap@zerosum.org>
2023-09-14 17:35:43 -0400

    update deploy script to work smoothly with wagmi (broadcast)

--- script/FourbyNFT.s.sol
+++ script/FourbyNFT.s.sol
@@ -2,11 +2,16 @@
 pragma solidity ^0.8.21;
 
 import {Script, console2} from "forge-std/Script.sol";
+import "../src/FourbyNFT.sol";
 
 contract FourbyScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+      uint256 deployerPrivateKey = vm.envUint("FORGE_PRIVATE_KEY");
+      address ownerAddress = vm.envAddress("FORGE_OWNER_ADDRESS");
+      vm.startBroadcast(deployerPrivateKey);
+      FourbyNFT nft = new FourbyNFT(ownerAddress);
+      vm.stopBroadcast();
     }
 }
